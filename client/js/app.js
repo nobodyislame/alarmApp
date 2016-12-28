fb5b193df71d3bd5259c69d72837e33d8ffae49c
vikram1804.prasad@gmail.com <vikram1804.prasad@gmail.com>
2016-12-28 18:16:50 +0530

    first commit for alarm app"

---
+++ client/js/app.js
@@ -0,0 +1,31 @@
+angular.module('alarm',['ui.router',
+						'alarm.controllers'])
+
+	.config(function( $stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state('current', {
+				url : '/current',
+				templateUrl : 'templates/currentTime.html',
+				controller : 'CurrentTimeCtrl'
+			})
+
+			.state('add', {
+				url : '/add',
+				templateUrl : 'templates/addAlarm.html',
+				controller : 'AddAlarmCtrl'
+			})
+
+			.state('total', {
+				url : '/total',
+				templateUrl : 'templates/totalAlarms.html',
+				controller : 'TotalAlarmsCtrl'
+			})
+
+			.state('about', {
+				url : '/about',
+				templateUrl : 'templates/about.html',
+				controller : 'AboutCtrl'
+			});
+
+		$urlRouterProvider.otherwise('/current');
+	})
---
+++ client/js/controllers.js
@@ -0,0 +1,70 @@
+angular.module('alarm.controllers',[])
+	
+	.controller('CurrentTimeCtrl', function($scope, $interval){
+		console.log('in current time control');
+		var time = function() {
+			var today = new Date();
+			$scope.hours = today.getHours();
+			$scope.minutes = today.getMinutes();
+			$scope.seconds = today.getSeconds();
+		}
+		time();
+		$interval(time, 1000);
+	})
+
+	.controller('AddAlarmCtrl', function($scope, $interval){
+		console.log('in add alarm ctrl');
+		$scope.input = {};
+		$scope.records = [];
+
+		var loadRemaining = function(alarm) {
+			var now = new Date();
+			var diff = alarm - now;
+			if(diff > 0) {
+				var remaining_hours = Math.round((diff / (1000 * 60 * 60)) % 24);
+				var remaining_mins = Math.round((diff / (1000 * 60)) % 60);
+				var remaining_secs = Math.round((diff / (1000)) % 60);
+				return remaining_hours + ":" + remaining_mins + ":" + remaining_secs;
+			}
+			return 'Completed'
+		}
+
+		$scope.add = function() {
+			var alarm_time = new Date();
+			alarm_time.setHours($scope.input.hr);
+			alarm_time.setMinutes($scope.input.min);
+			alarm_time.setSeconds($scope.input.sec);
+
+			var hrs = alarm_time.getHours();
+			var now = new Date();
+			
+			if(hrs < now.getHours()) {
+				alarm_time.setDate(alarm_time.getDate() + 1);
+			}
+
+			$scope.records.push({
+				alarm : alarm_time,
+				subject : $scope.input.subject,
+				remaining : loadRemaining(alarm_time)
+			});
+			
+			$scope.input.hr = '';
+			$scope.input.min = '';
+			$scope.input.sec = '';
+			$scope.input.subject = '';
+		}
+
+		$interval(
+			function(){
+				for(var i=0; i<$scope.records.length; i++) {
+					$scope.records[i].remaining = loadRemaining($scope.records[i].alarm);
+				}
+			},1000);
+	})
+	.controller('TotalAlarmsCtrl', function($scope){
+		console.log('in total alarms ctrl');
+	})
+
+	.controller('AboutCtrl', function($scope){
+		console.log('in about ctrl');
+	});
